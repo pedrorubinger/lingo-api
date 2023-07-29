ae8165656bd0d394aabc699bef2a9037a0b0d2f1
Pedro Rubinger <pedro.rubinger@gmail.com>
2023-07-29 16:58:27 -0300

    chore: add AI_BASE_URL const

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,6 +8,7 @@ dotenv.config()
 /* SERVER */
 export const PORT = process.env.PORT
 export const STAGE = process.env.STAGE as StageEnvironment
+export const AI_BASE_URL = OPENAI_BASE_URL
 
 /* OPEN AI */
-export { OpenAIConfiguration, OPENAI_BASE_URL }
+export { OpenAIConfiguration }
