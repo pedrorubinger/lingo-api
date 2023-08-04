9ea549beb2ededa92e0cb6b1a0a768f2be0c8ae4
Pedro Rubinger <pedro.rubinger@gmail.com>
2023-08-04 16:17:46 -0300

    chore: implements IController interface in CreateSentenceController class

--- src/infra/modules/interpreter/controllers/CreateSentenceController.ts
+++ src/infra/modules/interpreter/controllers/CreateSentenceController.ts
@@ -1,10 +1,13 @@
 import { Request, Response } from "express"
 
 import { container } from "@shared/ioc"
+import { IController } from "@core"
 import { CreateSentenceUseCase } from "@application/modules/interpreter/useCases"
 
-export class CreateSentenceController {
-  async handle(request: Request, response: Response) {
+export class CreateSentenceController
+  implements IController<Request, Response>
+{
+  async handle(request: Request, response: Response): Promise<Response> {
     const { prompt } = request.body
 
     const useCase = container.get(CreateSentenceUseCase)
