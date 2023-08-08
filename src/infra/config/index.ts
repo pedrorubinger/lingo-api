19f51433eec6f8292089da91b59daabc10ec6b2a
Pedro Rubinger <pedro.rubinger@gmail.com>
2023-08-08 16:00:22 -0300

    chore: remove unused env variable

--- src/infra/config/index.ts
+++ src/infra/config/index.ts
@@ -1,14 +1,13 @@
 import dotenv from "dotenv"
 
 import { StageEnvironment } from "@shared/types"
-import { OPENAI_BASE_URL, OpenAIConfiguration } from "@infra/config/openai"
+import { OpenAIConfiguration } from "@infra/config/openai"
 
 dotenv.config()
 
 /* SERVER */
 export const PORT = process.env.PORT
 export const STAGE = process.env.STAGE as StageEnvironment
-export const AI_BASE_URL = OPENAI_BASE_URL
 
 /* OPEN AI */
 export { OpenAIConfiguration }
--- src/infra/config/openai/index.ts
+++ src/infra/config/openai/index.ts
@@ -6,5 +6,3 @@ dotenv.config()
 export const OpenAIConfiguration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 })
-
-export const OPENAI_BASE_URL = process.env.OPENAI_BASE_URL
