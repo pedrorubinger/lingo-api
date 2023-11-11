54c707c4efdf1132f4eeb80e5bdd2bdcba952535
Pedro Rubinger <pedro.rubinger@gmail.com>
2023-11-11 00:44:10 -0300

    feat: add further test cases

--- test/unit/modules/interpreter/CreateSentenceUseCase.spec.ts
+++ test/unit/modules/interpreter/CreateSentenceUseCase.spec.ts
@@ -1,13 +1,19 @@
+import { Container } from "inversify"
 import "reflect-metadata"
+
 import { CreateSentenceUseCase } from "@/application/modules/interpreter/useCases"
 import { IChatService } from "@/application/services"
-// import { container } from "@/shared/ioc"
-import { TranslatorLanguageLabel } from "@/shared/types"
+import {
+  AppError,
+  ErrorCode,
+  TranslatorLanguageLabel,
+  left,
+} from "@/shared/types"
 import { ChatServiceMock } from "../../../mocks/ChatServiceMock"
-import { Container } from "inversify"
 
 describe("Interpreter > CreateSentenceUseCase", () => {
   const container = new Container()
+
   container.bind<IChatService>("ChatService").toConstantValue(ChatServiceMock)
   container.bind(CreateSentenceUseCase).toSelf()
 
@@ -26,6 +32,50 @@ describe("Interpreter > CreateSentenceUseCase", () => {
     if (result.isRight()) {
       expect(result.value.message).toBeDefined()
       expect(typeof result.value.message).toBe("string")
+    }
+  })
+
+  it("should throw an error when the message is not created", async () => {
+    expect.assertions(3)
+
+    ChatServiceMock.createTranslation.mockRejectedValue(
+      left(new AppError(ErrorCode.CHAT_SERVICE_NO_MESSAGE_RETURN, 400).get())
+    )
+
+    try {
+      await createSentenceUseCase.exec({
+        language: TranslatorLanguageLabel.ENGLISH,
+        sentence: "Lorem ipsum dolor",
+      })
+    } catch (err: any) {
+      expect(err.isRight()).toBeFalsy()
+
+      if (err.isLeft()) {
+        expect(err.value.code).toBe(ErrorCode.CHAT_SERVICE_NO_MESSAGE_RETURN)
+        expect(err.value.status).toBe(400)
+      }
+    }
+  })
+
+  it("should throw an error when something goes wrong", async () => {
+    expect.assertions(3)
+
+    ChatServiceMock.createTranslation.mockRejectedValue(
+      left(new AppError().get())
+    )
+
+    try {
+      await createSentenceUseCase.exec({
+        language: TranslatorLanguageLabel.ENGLISH,
+        sentence: "Lorem ipsum dolor",
+      })
+    } catch (err: any) {
+      expect(err.isRight()).toBeFalsy()
+
+      if (err.isLeft()) {
+        expect(err.value.code).toBe(ErrorCode.INTERNAL)
+        expect(err.value.status).toBe(500)
+      }
     }
   })
 })
